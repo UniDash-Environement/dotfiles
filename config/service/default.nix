8e33a90580541f07b2b3a48f2b1e4b9bc4a567f7
Pikatsuto <pikatsuto@gmail.com>
2023-08-01 12:28:09 +0200

    Fix: Missing in move service commit

--- config/service/default.nix
+++ config/service/default.nix
@@ -3,4 +3,19 @@
   imports = [
     ./src/nextcloud
   ];
+
+  services = {
+    gvfs.enable = true;
+    tumbler.enable = true;
+    openssh.enable = true;
+    gpm.enable = true;
+    upower.enable = true;
+  };
+
+  systemd.targets = {
+    sleep.enable = false;
+    suspend.enable = false;
+    hibernate.enable = false;
+    hybrid-sleep.enable = false;
+  };
 }
