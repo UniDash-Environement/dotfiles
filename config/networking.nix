326e95d6cdc35f2a5f8c26068d738756938b7b10
Pikatsuto <pikatsuto@gmail.com>
2023-09-29 22:38:36 +0200

    fix: Change IP

--- config/networking.nix
+++ config/networking.nix
@@ -16,5 +16,12 @@
       enableIPv6 = true;
       enable = true;
     };
+
+    interfaces.eno1.ipv4.addresses = [{
+      address = "45.88.180.18";
+      prefixLength = 24;
+    }];
+    defaultGateway = "45.88.180.254";
+    nameservers = [ "1.1.1.1" "1.0.0.1" ];
   };
 }
